4e16bfaead05f93293056a2e92208fa780d3f498
MelMuro <el_manuel_muro@hotmail.com>
2024-05-21 19:36:25 -0700

    Feature/add react query (#1)
    * Add react query library and sample
    
    * Add missing files and folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,38 @@
-import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { getRestaurantsQuery } from './api/sample-query';
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
-		<>
+		<QueryClientProvider client={new QueryClient()}>
 			<div className='card'>
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
+				<SampleComponent />
 			</div>
-		</>
+		</QueryClientProvider>
 	);
 }
+
+const SampleComponent = () => {
+	const { isPending, error, data } = getRestaurantsQuery();
+
+	if (isPending) {
+		return 'Loading Resturants data...';
+	}
+
+	if (error) {
+		return `An error has occurred while loading resturants data: ${error.message}`;
+	}
+
+	return (
+		<ul>
+			{data.map((restaurant) => {
+				return (
+					<li key={restaurant._id}>
+						{`Name: ${restaurant.name}, Description: ${restaurant.description}`}{' '}
+					</li>
+				);
+			})}
+		</ul>
+	);
+};
 
 export default App;
