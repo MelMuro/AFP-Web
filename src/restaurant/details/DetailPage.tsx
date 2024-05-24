b4519fdd55c6f5f411dc526d3c1867eef121749b
Jesús Iván Romero Valenzuela <iromero@necodex.com>
2024-05-24 10:18:18 -0700

    Replace using Semantic Elements

--- src/restaurant/details/DetailPage.tsx
+++ src/restaurant/details/DetailPage.tsx
@@ -7,43 +7,47 @@ import React from 'react';
 
 const DetailPage = () => {
 	return (
-		// Esto nos servira para despues, segun yo será para hacer los background dinamicos
-		<div
-			style={{ '--image-url': `url(${Fondo})` } as React.CSSProperties}
-			className='bg-[image:var(--image-url)]'
-		>
-			<div className='container my-2 py-5 mx-auto flex flex-row items-center text-white'>
-				<div className='basis-1/2 py-2 border-r-2 border-[#DD9E55]'>
-					<h2 className='font-jura font-bold'>City Wok</h2>
-					<h6 className='text-[#DD9E55]'>Comida China</h6>
-
-					<p className='font-montserrat w-3/5'>
-						Ingredientes frescos y recetas tradicionales, ofrecemos
-						variedad de platillos de todo tipo. Te invitamos a
-						pruebas nuestras especialidades
-					</p>
-
-					<div className='mt-5'>
-						<div className='flex items-center'>
-							<img src={Phone} alt='' />
-							<span>662 890 3344</span>
-						</div>
-						<div className='flex items-center'>
-							<img src={Mail} alt='' />
-							<a href=''>City@wok.mx</a>
+		<>
+			<section
+				style={
+					{ '--image-url': `url(${Fondo})` } as React.CSSProperties
+				}
+				className='bg-[image:var(--image-url)]'
+			>
+				<div className='container my-2 py-5 mx-auto flex flex-row items-center text-white'>
+					<div className='basis-1/2 py-2 border-r-2 border-[#DD9E55]'>
+						<h2 className='font-jura font-bold'>City Wok</h2>
+						<span className='text-[#DD9E55]'>Comida China</span>
+						<p className='font-montserrat w-3/5'>
+							Ingredientes frescos y recetas tradicionales,
+							ofrecemos variedad de platillos de todo tipo. Te
+							invitamos a pruebas nuestras especialidades
+						</p>
+						<ul className='mt-5'>
+							<li className='flex items-center'>
+								<img src={Phone} alt='Phone' />
+								<a href='tel:6628903344'>662 890 3344</a>
+							</li>
+							<li className='flex items-center'>
+								<img src={Mail} alt='Email' />
+								<a href='mailto:hola@hotmail.com'>
+									City@wok.mx
+								</a>
+							</li>
+						</ul>
+						<div className='flex mt-10'>
+							<img className='mr-3' src={Face} alt='Facebook' />
+							<img className='ml-3' src={Insta} alt='Instagram' />
 						</div>
 					</div>
-					<div className='flex mt-10'>
-						<img className='mr-3' src={Face} alt='' />
-						<img className='ml-3' src={Insta} alt='' />
+					<div className='basis-1/2 ml-5'>
+						<h2 className='text-[#DD9E55] font-montserrat'>
+							Horarios
+						</h2>
 					</div>
 				</div>
-				<div className='basis-1/2 ml-5'>
-					{/* Aqui va el componente de Horarios */}
-					<h2 className='text-[#DD9E55] font-montserrat'>Horarios</h2>
-				</div>
-			</div>
-		</div>
+			</section>
+		</>
 	);
 };
 
