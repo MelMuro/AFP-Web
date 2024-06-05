85eeea058facaaf696a6dbbd5959a0a2a442f195
Ivan <49807747+Chuvan98@users.noreply.github.com>
2024-06-05 11:13:06 -0700

    Separate form contact to another component  (#8)
    * relocate detail page to folder restaurants
    
    * Put form contact in a diferent component
    
    * Add global style and import footer to app
    
    ---------
    
    Co-authored-by: Jesús Iván Romero Valenzuela <iromero@necodex.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import DetailPage from './restaurant/details/DetailPage';
+import DetailPage from './restaurants/details/DetailPage';
+import './global-styles.css';
+import Footer from './common/footer/Footer';
 
 function App() {
 	return (
 		<QueryClientProvider client={new QueryClient()}>
 			<div className='card'>
 				<DetailPage />
+				<Footer />
 			</div>
 		</QueryClientProvider>
 	);
--- src/common/footer/Footer.tsx
+++ src/common/footer/Footer.tsx
@@ -1,6 +1,7 @@
-import fog from '../assets/dobleFog.png';
-import Face from '../assets/Face.svg';
-import Insta from '../assets/Insta.svg';
+import fog from '../../assets/dobleFog.png';
+import Face from '../../assets/Face.svg';
+import Insta from '../../assets/Insta.svg';
+import FormContact from './FormContact';
 
 const Footer = () => {
 	return (
@@ -9,87 +10,7 @@ const Footer = () => {
 				<div className=' container mx-auto'>
 					<div className='flex flex-row'>
 						<div className='w-1/2'>
-							<form className='mr-12 '>
-								<label
-									htmlFor='contact'
-									className='my-4 labelForm text-golden'
-								>
-									¿Negocio o dudas?
-								</label>
-								<br />
-								<select
-									name='contact'
-									id='contact'
-									className='placeholderForm p-2 my-2 border-2 border-solid border-golden text-gray w-full'
-								>
-									<option
-										value=''
-										selected
-										className='text-gray'
-									>
-										Asunto
-									</option>
-									<option
-										value='business'
-										className='text-gray'
-									>
-										Negocio
-									</option>
-									<option value='hi' className='text-gray'>
-										Saludar
-									</option>
-								</select>
-								<br />
-								<label
-									htmlFor='name'
-									className='labelForm text-golden'
-								>
-									Ingresa tu nombre
-								</label>
-								<br />
-								<input
-									type='text'
-									name='name'
-									id='name'
-									placeholder='Ingresa el nombre de tu negocio'
-									className='placeholderForm p-2 my-2 border-2 border-solid border-golden text-gray w-full'
-								/>
-								<br />
-								<label
-									htmlFor='client'
-									className='labelForm text-golden'
-								>
-									Número de teléfono o correo
-								</label>
-								<input
-									type='text'
-									name='client'
-									id='client'
-									placeholder='Contacto'
-									className='placeholderForm p-2 my-2 border-2 border-solid border-golden text-gray w-full'
-								/>
-								<br />
-								<label
-									htmlFor='message'
-									className='labelForm text-golden'
-								>
-									Mensaje
-								</label>
-								<textarea
-									name='message'
-									id='message'
-									rows={4}
-									placeholder='Hablanos de ti...'
-									className='placeholderForm my-2 p-2 border-2 border-solid border-golden text-gray w-full'
-								></textarea>
-								<br />
-								<button
-									type='submit'
-									className='mt-5 labelForm bg-golden w-full h-10'
-								>
-									Enviar mensaje
-								</button>
-							</form>
+							<FormContact />
 						</div>
 						<div className='w-1/2'>
 							<h1 className='text-white'>Entremos en contacto</h1>
