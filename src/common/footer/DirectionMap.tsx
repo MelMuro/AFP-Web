40958fdf3a4f854848f50d1592d174ced3a33260
Jesús Iván Romero Valenzuela <iromero@necodex.com>
2024-06-05 11:34:34 -0700

    Create direction map component

---
+++ src/common/footer/DirectionMap.tsx
@@ -0,0 +1,11 @@
+import Map from '../../assets/borrarMapa.png';
+
+const DirectionMap = () => {
+	return (
+		<div>
+			<img className='bg-cover w-full h-1/6' src={Map} alt='' />
+		</div>
+	);
+};
+
+export default DirectionMap;
