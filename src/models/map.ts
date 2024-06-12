0f4483272e93bd3464af103a84f486b0cc8ab2b5
Karumuu <kalm.vorcana@gmail.com>
2024-06-12 15:03:43 -0700

    Add react google map and custom marker

---
+++ src/models/map.ts
@@ -0,0 +1,38 @@
+declare module '@vis.gl/react-google-maps' {
+	import { CSSProperties, ReactNode, ComponentType } from 'react';
+
+	type MapProps = {
+		mapId?: string;
+		style?: CSSProperties;
+		defaultCenter?: { lat: number; lng: number };
+		defaultZoom?: number;
+		gestureHandling?: string;
+		disableDefaultUI?: boolean;
+		children?: ReactNode;
+	};
+
+	type APIProviderProps = {
+		apiKey: string;
+		children?: ReactNode;
+	};
+
+	type MarkerProps = {
+		position: { lat: number; lng: number };
+	};
+
+	type AdvancedMarker = {
+		position: { lat: number; lng: number };
+		children?: ReactNode;
+	};
+
+	type InfoWindow = {
+		position: { lat: number; lng: number };
+		children?: ReactNode;
+	};
+
+	export const Map: ComponentType<MapProps>;
+	export const APIProvider: ComponentType<APIProviderProps>;
+	export const Marker: ComponentType<MarkerProps>;
+	export const AdvancedMarker: ComponentType<AdvancedMarker>;
+	export const InfoWindow: ComponentType<InfoWindow>;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@vis.gl/react-google-maps': '@vis.gl/react-google-maps'
+		}
+	}
+});
