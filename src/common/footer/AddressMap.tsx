1c607e728d7bf8aff360f742e409cc49d9ee94b9
Nolan <123855960+Nolan-mjns@users.noreply.github.com>
2024-06-12 19:39:34 -0700

    Add react google map and custom marker (#13)
    * Add react google map and custom marker
    
    * "alias react google maps" removed

--- src/common/footer/AddressMap.tsx
+++ src/common/footer/AddressMap.tsx
@@ -1,9 +1,32 @@
-import Map from '../../assets/borrarMapa.png';
+import { useState, FunctionComponent } from 'react';
+import { APIProvider, Map, AdvancedMarker } from '@vis.gl/react-google-maps';
+import pinMarker from '../../assets/pinFavIcon.svg';
 
-const AddressMap = () => {
+const AddressMap: FunctionComponent<Record<string, unknown>> = () => {
+	const [coords] = useState<{ lat: number; lng: number }>({
+		lat: 29.10592366015792,
+		lng: -110.9673906377404
+	});
+
+	const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+	const mapId = import.meta.env.VITE_GOOGLE_MAPS_MAP_ID;
+
 	return (
 		<div>
-			<img className='bg-cover w-full h-1/6' src={Map} alt='' />
+			<APIProvider apiKey={apiKey}>
+				<Map
+					mapId={mapId}
+					style={{ width: '100%', height: '80vh' }}
+					defaultCenter={coords}
+					defaultZoom={18}
+					gestureHandling={'greedy'}
+					disableDefaultUI={false}
+				>
+					<AdvancedMarker position={coords}>
+						<img src={pinMarker} width={50} height={50} />
+					</AdvancedMarker>
+				</Map>
+			</APIProvider>
 		</div>
 	);
 };
