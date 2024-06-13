c760c53054130354382dcd03d595ab68edaacf4d
Karumuu <kalm.vorcana@gmail.com>
2024-06-12 19:38:29 -0700

    "alias react google maps" removed

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			'@vis.gl/react-google-maps': '@vis.gl/react-google-maps'
-		}
-	}
+	plugins: [react()]
 });
