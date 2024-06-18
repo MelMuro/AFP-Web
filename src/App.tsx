c8abdce45a46763a45461ff17143f8f891999321
MelMuro <el_manuel_muro@hotmail.com>
2024-06-18 12:19:48 -0700

    Add react router basic configuration (#14)
    * Add router provider
    
    * Add route sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import DetailPage from './restaurants/details/DetailPage';
 import './global-styles.css';
 import Footer from './common/footer/Footer';
+import { Outlet } from 'react-router-dom';
 
-function App() {
+const App = () => {
 	return (
 		<QueryClientProvider client={new QueryClient()}>
 			<div className='card'>
-				<DetailPage />
+				<Outlet />
 				<Footer />
 			</div>
 		</QueryClientProvider>
 	);
-}
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './home/Home.tsx';
 
-
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+		children: [
+			{
+				path: '/home',
+				element: <Home />
+			}
+		]
+	}
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-	
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>
 );
