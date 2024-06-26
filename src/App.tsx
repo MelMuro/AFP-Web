2803a83d0077509b6360d5daaba1cab0e2732290
Nolan <123855960+Nolan-mjns@users.noreply.github.com>
2024-06-26 15:15:41 -0700

    About section (#16)
    * Add react google map and custom marker
    
    * "alias react google maps" removed
    
    * About seccion + new images

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './global-styles.css';
 import Footer from './common/footer/Footer';
+import Home from './home/Home';
 import { Outlet } from 'react-router-dom';
 
 const App = () => {
 	return (
 		<QueryClientProvider client={new QueryClient()}>
+			<Home />
 			<div className='card'>
 				<Outlet />
 				<Footer />
