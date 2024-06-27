14160c32ea7c45ce7a43a547e1c770d265def145
Karumuu <kalm.vorcana@gmail.com>
2024-06-27 16:56:05 -0700

    tailwind adjustments

--- src/common/NotFound.tsx
+++ src/common/NotFound.tsx
@@ -3,28 +3,26 @@ import Torii from '../assets/Torii.jpg';
 
 export default function NotFound() {
 	return (
-		<section className='bg-gray-100 flex items-center justify-center h-screen'>
+		<section className='flex items-center justify-center h-screen font-jura'>
 			<div className='text-center'>
 				<div className='flex justify-center items-center'>
 					<img className='w-1/3' src={Torii} />
 				</div>
 
-				<h1 className='text-6xl font-bold text-gray-800 mb-4 mt-6'>
-					404
-				</h1>
-				<p className='text-xl text-gray-600 mb-8'>
-					¡Oops! Parece que te has cruzado la puerta Torii equivocada.
+				<h1 className='text-6xl font-bold mb-4 mt-6'>404</h1>
+				<p className='text-xl mb-8'>
+					¡Oops! Parece que has cruzado la puerta Torii equivocada.
 				</p>
 				<div className='space-x-4'>
 					<a
 						href='#'
-						className='text-redDefault border border-y-redDefault px-4 py-2  hover:bg-blue-600'
+						className='text-redDefault border border-redDefault px-4 py-2 font-bold'
 					>
 						Volver a inicio
 					</a>
 					<a
 						href='#'
-						className='text-redDefault border border-y-redDefault px-4 py-2  hover:bg-green-600'
+						className='text-blackDefault border border-golden bg-golden px-4 py-2 font-bold'
 					>
 						Restaurantes
 					</a>
