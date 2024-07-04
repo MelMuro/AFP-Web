7a5768215a308156ad33d3417a4c2c78940458d8
Ivan <49807747+Chuvan98@users.noreply.github.com>
2024-07-04 10:11:07 -0700

    First view of Restaurants  (#15)
    * Fist restaurants view
    
    * First view Carrusel CSS and images
    
    * Desing carousel right
    
    * Carousel left
    
    * Little adjustment of css
    
    * Left new view
    
    * Desing update
    
    * Grid position slider adjustments
    
    * CSS gradient and arrows carousel
    
    * Split into components
    
    * Remove Console Carousel
    
    ---------
    
    Co-authored-by: Jesús Iván Romero Valenzuela <iromero@necodex.com>

---
+++ src/restaurants/overview/Carousel.tsx
@@ -0,0 +1,50 @@
+import Comida1 from '../../assets/Comida1.png';
+import Comida2 from '../../assets/Comida2.png';
+import InfoCarousel from './InfoCarousel';
+import ImagesCarousel from './ImagesCarousel';
+
+const slides: Slide[] = [
+	{
+		image: Comida1,
+		title: 'Arroz frito especial',
+		description: 'Ingredientes frescos y recetas tradicionales...'
+	},
+	{
+		image: Comida2,
+		title: 'Otro plato',
+		description: 'Descripción del otro plato...'
+	}
+];
+
+const Carousel: React.FC = () => {
+	return (
+		<>
+			{slides.map((_, index) => {
+				const direction = index % 2 === 0;
+				return (
+					<>
+						{!direction ? (
+							<div className='flex justify-center items-center relative h-fit pb-20'>
+								<InfoCarousel direction={direction} />
+								<ImagesCarousel
+									direction={direction}
+									slides={slides}
+								/>
+							</div>
+						) : (
+							<div className='flex justify-center items-center relative h-fit'>
+								<ImagesCarousel
+									direction={direction}
+									slides={slides}
+								/>
+								<InfoCarousel direction={direction} />
+							</div>
+						)}
+					</>
+				);
+			})}
+		</>
+	);
+};
+
+export default Carousel;
---
+++ src/restaurants/overview/Restaurants.tsx
@@ -0,0 +1,39 @@
+import dobleFog from '../../assets/dobleFog.png';
+import Nube1 from '../../assets/Nube1.png';
+import Carousel from './Carousel';
+const Restaurants = () => {
+	return (
+		<>
+			<section className='relative bg-cover bg-ink bg-bottom h-full'>
+				<div className='flex items-center'>
+					<div className='w-1/2 px-14 py-44'>
+						<h1 className='text-white'>De Asia a tu mesa</h1>
+						<p className='text-white w-1/2'>
+							Desde sabores picantes hasta dulces, bienvenido a
+							Asia Yá, conoce nuestra variedad de restaurantes
+							donde podrás descubrir nuevos sabores o disfrutar de
+							tus favoritos llevados a otro nivel.
+						</p>
+						<img
+							src={Nube1}
+							alt=''
+							className='w-1/4 absolute -left-28'
+						/>
+					</div>
+					<div className='w-1/2'>
+						<img
+							src={dobleFog}
+							alt=''
+							className='w-1/3 absolute -right-5 -mt-32'
+						/>
+					</div>
+				</div>
+			</section>
+			<section className='grid grid-cols-2 mx-24 my-20'>
+				<Carousel></Carousel>
+			</section>
+		</>
+	);
+};
+
+export default Restaurants;
