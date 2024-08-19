c1cfa3680dc63e95f4f82aef440d8e22d7823ad4
Jesús Iván Romero Valenzuela <iromero@necodex.com>
2024-08-19 09:36:06 -0700

    Img Gyoza better position

--- src/home/components/Schedule.tsx
+++ src/home/components/Schedule.tsx
@@ -5,11 +5,13 @@ import gyoza from '../../assets/gyoza 1.png';
 export default function Schedule() {
 	return (
 		<>
-			<section className='relative pb-40 xl:px-12 schedule xl:pr-28 xl:pl-28 md:p-5 md:mx-5'>
-				<div className='lg:p-10 border-redDefault lg:w-2/5 border-2 schedul'>
+			<section className='relative pb-40 xl:px-12 xl:pb-72 schedule xl:pr-28 xl:pl-28 md:p-5 md:mx-5'>
+				<div className='lg:p-10 border-redDefault lg:w-2/5 border-2 schedule'>
 					<h2>Horario</h2>
-					<p className='infoP'>Lunes a Viernes - 12:00pm a 10:00pm</p>
-					<p className='infoP'>
+					<p className='infoP mr-20 2xl:mr-0'>
+						Lunes a Viernes - 12:00pm a 10:00pm
+					</p>
+					<p className='infoP mr-20 2xl:mr-0'>
 						Sabados y Domingos - 12:00pm a 12:00am
 					</p>
 
