649d7d633e8b0a78afebd1b52d0743d88fc1a5ff
Ivan <49807747+Chuvan98@users.noreply.github.com>
2024-08-21 16:49:26 -0700

    Loading spinner (#26)
    Co-authored-by: Jesús Iván Romero Valenzuela <iromero@necodex.com>

---
+++ src/common/Loader/Loader.tsx
@@ -0,0 +1,12 @@
+import './loader.css';
+const Loader = () => {
+	return (
+		<>
+			<div className='loader-overlay'>
+				<span className='loader'></span>
+			</div>
+		</>
+	);
+};
+
+export default Loader;
