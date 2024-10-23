3b331a2c03291ef58ea457cadf116a882536e48e
Jesús Iván Romero Valenzuela <iromero@necodex.com>
2024-10-23 14:47:54 -0700

    Test change page url

--- src/restaurants/details/DetailPage.test.tsx
+++ src/restaurants/details/DetailPage.test.tsx
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
 import DetailPage from './DetailPage';
 import { renderWithClient } from '../../test/test-utils';
-import { MemoryRouter, Route, Routes } from 'react-router-dom';
-
+import { MemoryRouter, Route, Router, Routes } from 'react-router-dom';
+import Restaurants from '../overview/Restaurants';
 describe('DetailPage', () => {
 	it('renders the restaurant name', async () => {
 		const result = renderWithClient(
@@ -26,5 +26,22 @@ describe('DetailPage', () => {
 		expect(
 			await result.findByText('Sabado: 8:00 - 17:00')
 		).toBeInTheDocument();
+	});
+
+	it('Click a restaurant and load detail pages', async () => {
+		const result = renderWithClient(
+			<MemoryRouter initialEntries={['/restaurants']}>
+				<Routes>
+					<Route path='restaurants' element={<Restaurants />} />
+					<Route path='/details/:name' element={<DetailPage />} />
+				</Routes>
+			</MemoryRouter>
+		);
+		expect(await result.getByText('De Asia a tu mesa')).toBeInTheDocument();
+		expect(await result.findByText('Restaurant 1')).toBeInTheDocument();
+
+		const fistButton = await result.findAllByText('Ver menú');
+		fireEvent.click(fistButton[0]);
+		expect(await result.findByText('Horarios')).toBeInTheDocument();
 	});
 });
