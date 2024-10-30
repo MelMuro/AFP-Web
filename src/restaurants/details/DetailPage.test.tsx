e85ab41e8e6ad7ee18f2c490af93e5a941d3320f
Ivan <49807747+Chuvan98@users.noreply.github.com>
2024-10-30 14:38:34 -0700

    Little test in resturants and detail restaurant (#34)
    * Setup new monk data and first test
    
    * Fix images Slider
    
    * Text click and prev click
    
    * Test details restaurant
    
    * Test change page url
    
    * Update using destructuring
    
    ---------
    
    Co-authored-by: Jesús Iván Romero Valenzuela <iromero@necodex.com>

--- src/restaurants/details/DetailPage.test.tsx
+++ src/restaurants/details/DetailPage.test.tsx
@@ -1,15 +1,47 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
 import DetailPage from './DetailPage';
 import { renderWithClient } from '../../test/test-utils';
-import { MemoryRouter } from 'react-router-dom';
-
+import { MemoryRouter, Route, Router, Routes } from 'react-router-dom';
+import Restaurants from '../overview/Restaurants';
 describe('DetailPage', () => {
 	it('renders the restaurant name', async () => {
 		const result = renderWithClient(
-			<MemoryRouter>
+			<MemoryRouter initialEntries={['/details/Restaurant%201']}>
+				<Routes>
+					<Route path='/details/:name' element={<DetailPage />} />
+				</Routes>
 				<DetailPage />
 			</MemoryRouter>
 		);
-		expect(await result.findByAltText('Restaurant 1')).toBeInTheDocument();
+
+		expect(await result.findByText('Restaurant 1')).toBeInTheDocument();
+		expect(
+			await result.findByText('Some test description')
+		).toBeInTheDocument();
+
+		expect(
+			await result.findByText('Lunes: 8:00 - 17:00')
+		).toBeInTheDocument();
+
+		expect(
+			await result.findByText('Sabado: 8:00 - 17:00')
+		).toBeInTheDocument();
+	});
+
+	it('Click a restaurant and load detail pages', async () => {
+		const result = renderWithClient(
+			<MemoryRouter initialEntries={['/restaurants']}>
+				<Routes>
+					<Route path='restaurants' element={<Restaurants />} />
+					<Route path='/details/:name' element={<DetailPage />} />
+				</Routes>
+			</MemoryRouter>
+		);
+		expect(await result.getByText('De Asia a tu mesa')).toBeInTheDocument();
+		expect(await result.findByText('Restaurant 1')).toBeInTheDocument();
+
+		const [fistButton] = await result.findAllByText('Ver menú');
+		fireEvent.click(fistButton);
+		expect(await result.findByText('Horarios')).toBeInTheDocument();
 	});
 });
--- src/restaurants/overview/Restaurants.test.tsx
+++ src/restaurants/overview/Restaurants.test.tsx
@@ -1,6 +1,7 @@
 import Restaurants from './Restaurants';
 import { renderWithClient } from '../../test/test-utils';
 import { MemoryRouter } from 'react-router-dom';
+import { fireEvent } from '@testing-library/dom';
 
 describe('Restaurants', () => {
 	it('renders restaurants components', async () => {
@@ -10,7 +11,41 @@ describe('Restaurants', () => {
 			</MemoryRouter>
 		);
 
-		expect(await result.findByAltText('Restaurant 1')).toBeInTheDocument();
-		expect(await result.findByAltText('Restaurant 2')).toBeInTheDocument();
+		expect(await result.findByText('Restaurant 1')).toBeInTheDocument();
+		expect(await result.getByText('Restaurant 2')).toBeInTheDocument();
+
+		expect(
+			await result.getByText('Some test description')
+		).toBeInTheDocument();
+	});
+
+	it('Click next button Carousel section', async () => {
+		const result = renderWithClient(
+			<MemoryRouter>
+				<Restaurants />
+			</MemoryRouter>
+		);
+
+		expect(await result.findByAltText('chilaquiles')).toBeInTheDocument();
+
+		const [slideButtonNext] = result.getAllByAltText('next');
+		fireEvent.click(slideButtonNext);
+
+		expect(await result.findByAltText('carlota')).toBeInTheDocument();
+	});
+
+	it('Click prev button Carousel section', async () => {
+		const result = renderWithClient(
+			<MemoryRouter>
+				<Restaurants />
+			</MemoryRouter>
+		);
+
+		expect(await result.findByAltText('Espaguetti')).toBeInTheDocument();
+
+		const slideButtonsPrev = result.getAllByAltText('prev');
+		fireEvent.click(slideButtonsPrev[1]);
+
+		expect(await result.findByAltText('Sake')).toBeInTheDocument();
 	});
 });
